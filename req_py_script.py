fa1a74962fe1e73301865d57b02e8c1a45c3b6f4
AjayKumar9375 <93271770+AjayKumar9375@users.noreply.github.com>
2024-07-02 09:52:19 +0530

    Update req_py_script.py

--- req_py_script.py
+++ req_py_script.py
@@ -18,7 +18,7 @@ def create_file(filename):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Modify and read the content of a .txt file.')
-    parser.add_argument("--filename", required=true , help='The path to the .txt file to modify and read.')
+    parser.add_argument("--filename", required=True , help='The path to the .txt file to modify and read.')
     args = parser.parse_args()
     
     file_content = create_file(args.filename)
