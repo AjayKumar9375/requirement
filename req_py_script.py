5e362828553d4d676ee1bbe98e12375749486fa7
AjayKumar9375 <93271770+AjayKumar9375@users.noreply.github.com>
2024-07-02 09:29:33 +0530

    Update req_py_script.py

--- req_py_script.py
+++ req_py_script.py
@@ -1,13 +1,28 @@
-import sys
+import argparse
 
 def create_file(filename):
     with open(filename, 'w') as file:
-        file.write("Hello, TeamCity!")
+        lines = file.readlines()
+
+    new_requirement = "new_package/1.0.2/release"
+    if new_requirement not in lines:
+        lines.append(new_requirement)
+
+    with open(filename, 'w') as file:
+        file.writelines(lines)
+
+    with open(filename, 'r') as file:
+        modified_file = file.read()
+
+    return modified_file
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python create_file.py <output_filename>")
-        sys.exit(1)
+    parser = argparse.ArgumentParser(description='Modify and read the content of a .txt file.')
+    parser.add_argument('filename', type=str, help='The path to the .txt file to modify and read.')
+    args = parser.parse_args()
     
-    output_filename = sys.argv[1]
-    create_file(output_filename)
+    file_content = create_file(output_filename)
+
+    print("modified content")
+    print(file_content)
+    
