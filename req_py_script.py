a8f0b0f726efa4ec912ec7a6c2f766ff430dcede
AjayKumar9375 <93271770+AjayKumar9375@users.noreply.github.com>
2024-07-02 10:31:36 +0530

    Update req_py_script.py

--- req_py_script.py
+++ req_py_script.py
@@ -7,17 +7,6 @@ def create_file(filename, package_name):
         if line.startswith(package_name):
             return True
     return False
-    # new_requirement = "new_package/1.0.2/release"
-    # if new_requirement not in lines:
-    #     lines.append(new_requirement)
-
-    # with open(filename, 'w') as file:
-    #     file.writelines(lines)
-
-    # with open(filename, 'r') as file:
-    #     modified_file = file.read()
-
-    # return modified_file
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Modify and read the content of a .txt file.')
@@ -27,6 +16,6 @@ if __name__ == "__main__":
     
     file_content = create_file(args.filename, args.package_name)
 
-    print("modified content")
+    print("Status")
     print(file_content)
     
