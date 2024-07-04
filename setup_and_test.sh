736bbee2ac22684bfe95187c4f9bc0cf3f566247
AjayKumar9375 <93271770+AjayKumar9375@users.noreply.github.com>
2024-07-04 12:44:37 +0530

    Update setup_and_test.sh

--- setup_and_test.sh
+++ setup_and_test.sh
@@ -5,7 +5,7 @@ python -m venv venv       # Create a new virtual environment named 'venv'
 source venv/bin/activate  # Activate the virtual environment
 
 # Install dependencies
-pip install -r requirements.txt  # Install Python dependencies listed in 'requirements.txt'
+pip install -r requirements1.txt  # Install Python dependencies listed in 'requirements.txt'
 
 # Run tests
 pytest  # Run tests using pytest
